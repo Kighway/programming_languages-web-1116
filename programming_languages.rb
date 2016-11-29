a1760a4f5f55355d789538049da6b0e4c265d1be
Kyle Tulod <tulod.ky13@gmail.com>
2016-11-29 01:19:40 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,14 @@
-def reformat_languages(languages)
-  # your code here
+def reformat_languages(styles)
+  reformatted_languages = {}
+  styles.each do |style, languages|
+    languages.each do |language, type_hash|
+      type = type_hash[:type]
+      if reformatted_languages.include? (language)
+        reformatted_languages[language][:style].push(style) unless reformatted_languages[language][:style].include?(style)
+      else
+        reformatted_languages[language] = { :style => [style], :type => type }
+      end
+    end
+  end
+  reformatted_languages
 end
